6b15511c686978a48af898ff9c59df0da405f06e
Pawel Reich <thegitman@outlook.com>
2024-05-16 01:46:36 +0200

    Testing various RayGui possibilities

--- Puma Visualiser/Puma Visualiser/Visualiser.cs
+++ Puma Visualiser/Puma Visualiser/Visualiser.cs
@@ -12,10 +12,11 @@ public static class Visualiser
 
 	public static async Task Main(string[] args)
 	{
+		Raylib.SetConfigFlags(ConfigFlags.FLAG_WINDOW_RESIZABLE);
 		Raylib.InitWindow(1280, 720, "PUMA Robot Visualiser");
 		Raylib.SetTargetFPS(60);
 
-		while (!Raylib.WindowShouldClose()) // Detect window close button or ESC key
+		while (!Raylib.WindowShouldClose())
 		{
 			Raylib.BeginDrawing();
 
@@ -32,7 +33,6 @@ public static class Visualiser
 				default:
 					throw new NotImplementedException("Unsupported AppState!");
 			}
-
 			Raylib.EndDrawing();
 		}
 		Raylib.CloseWindow();
@@ -49,8 +49,34 @@ public static class Visualiser
 		}
 	}
 
+	private static bool test = false;
+	private static string x = "";
+
 	private static void RunApp()
 	{
 		Raylib.ClearBackground(Raylib.LIGHTGRAY);
+		test = RayGui.GuiCheckBox(new Rectangle(10, 10, 20, 20), "test ok?", test);
+
+		int RightPanelStart = Raylib.GetScreenWidth() - 250;
+
+		Raylib.DrawLine(RightPanelStart, 0, RightPanelStart, Raylib.GetScreenHeight(), Raylib.BLACK);
+
+		char cha = (char)Raylib.GetCharPressed();
+		int key = Raylib.GetKeyPressed();
+		if (cha != 0)
+		{
+			Console.WriteLine($"Char Pressed : {cha}");
+			x += cha;
+		}
+		else if (key != 0)
+		{
+			Console.WriteLine($"Pressed : {key}");
+			if (key == 259)
+			{
+				x = x.Remove(x.Length - 1);
+			}
+		}
+
+		RayGui.GuiTextBox(new Rectangle(RightPanelStart, 10, 230, 30), x, 20, true);
 	}
 }

081383de595be7305227430000ba56843b58a7b5
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2024-05-18 17:12:46 +0200

    Created Label GuiElement

--- Puma Visualiser/Puma Visualiser/Views/MainView.cs
+++ Puma Visualiser/Puma Visualiser/Views/MainView.cs
@@ -9,13 +9,17 @@ namespace Puma_Visualiser.Views
 
 		private int RightPanelStart = -250;
  
-		private List<TextBox> _textBoxes = new List<TextBox>();
+		private List<GuiElement> _textBoxes = new List<GuiElement>();
 
 		public MainView()
 		{
 			_textBoxes.Add(new TextBox(new Rectangle(RightPanelStart + 40, 10, 200, 30), 20));
 			_textBoxes.Add(new TextBox(new Rectangle(RightPanelStart + 40, 50, 200, 30), 20));
 			_textBoxes.Add(new TextBox(new Rectangle(RightPanelStart + 40, 90, 200, 30), 20));
+
+			_textBoxes.Add(new Label(new Rectangle(RightPanelStart + 10, 10 + 5, 0, 0), "X", 20, Raylib.BLACK));
+			_textBoxes.Add(new Label(new Rectangle(RightPanelStart + 10, 50 + 5, 0, 0), "Y", 20, Raylib.BLACK));
+			_textBoxes.Add(new Label(new Rectangle(RightPanelStart + 10, 90 + 5, 0, 0), "Z", 20, Raylib.BLACK));
 		}
 
 		public void Draw()
@@ -23,10 +27,6 @@ namespace Puma_Visualiser.Views
 			Raylib.ClearBackground(Raylib.LIGHTGRAY);
 			test = RayGui.GuiCheckBox(new Rectangle(10, 10, 20, 20), "test ok?", test);
 
-
-			Raylib.DrawText("X", RightPanelStart + 10, 10 + 5, 20, Raylib.BLACK);
-			Raylib.DrawText("Y", RightPanelStart + 10, 50 + 5, 20, Raylib.BLACK);
-			Raylib.DrawText("Z", RightPanelStart + 10, 90 + 5, 20, Raylib.BLACK);
 			Raylib.DrawRectangle(Raylib.GetScreenWidth() + RightPanelStart, 0, 250, Raylib.GetScreenHeight(), Raylib.WHITE);
 
 			foreach (var item in _textBoxes)

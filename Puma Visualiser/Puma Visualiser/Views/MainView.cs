9eb84deee9583f48f91d6fef4e1c5a84c07f54ce
Paweł Reich <37516105+Gitmanik@users.noreply.github.com>
2024-05-19 19:31:01 +0200

    Cosmetic changes in MainView

--- Puma Visualiser/Puma Visualiser/Views/MainView.cs
+++ Puma Visualiser/Puma Visualiser/Views/MainView.cs
@@ -6,48 +6,65 @@ namespace Puma_Visualiser.Views;
 internal class MainView : IView
 {
     private int RightPanelStart = -250;
+    private int margin = 10;
     private int RightPanelInputStartY = 100;
 
     private List<GuiElement> _textBoxes = new List<GuiElement>();
 
     private Timeline _timeline;
-    
+
     private float _time;
     private float _timeEnd = 5;
     private float _timeStart = 0;
     private float _timeScale = 1;
-    
+
     public MainView()
     {
-        _timeline = new Timeline(new Rectangle(0, -250, -250, 250), Raylib.GRAY, Raylib.LIGHTGRAY, Raylib.GRAY, Raylib.RED, 10, 10);
-        _textBoxes.Add(new TextBox(new Rectangle(RightPanelStart + 40, RightPanelInputStartY + 10, 200, 30), 20));
-        _textBoxes.Add(new TextBox(new Rectangle(RightPanelStart + 40, RightPanelInputStartY + 50, 200, 30), 20));
-        _textBoxes.Add(new TextBox(new Rectangle(RightPanelStart + 40, RightPanelInputStartY + 90, 200, 30), 20));
-        _textBoxes.Add(new TextBox(new Rectangle(RightPanelStart + 40, RightPanelInputStartY + 130, 200, 30), 20));
+        _timeline = new Timeline(new Rectangle(0, -250, -250, 250), Raylib.GRAY, Raylib.LIGHTGRAY, Raylib.DARKGRAY,
+            Raylib.RED, 10, 10);
+        _textBoxes.Add(new TextBox(
+            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 10, 200 - 2 * margin, 30), 20));
+        _textBoxes.Add(new TextBox(
+            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 50, 200 - 2 * margin, 30), 20));
+        _textBoxes.Add(new TextBox(
+            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 90, 200 - 2 * margin, 30), 20));
+        _textBoxes.Add(new TextBox(
+            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 130, 200 - 2 * margin, 30), 20));
 
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + 10, RightPanelInputStartY + 10 + 5, 0, 0), "X", 20,
+        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 10 + 5, 0, 0),
+            "X", 20,
             Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + 10, RightPanelInputStartY + 50 + 5, 0, 0), "Y", 20,
+        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 50 + 5, 0, 0),
+            "Y", 20,
             Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + 10, RightPanelInputStartY + 90 + 5, 0, 0), "Z", 20,
+        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 90 + 5, 0, 0),
+            "Z", 20,
             Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + 10, RightPanelInputStartY + 130 + 5, 0, 0), "t", 20,
+        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 130 + 5, 0, 0),
+            "t", 20,
             Raylib.BLACK));
     }
 
     public void Draw()
     {
+        //TODO: Instead draw Raylib.GREY
         Raylib.ClearBackground(Raylib.LIGHTGRAY);
+        Raylib.DrawRectangle(0, 0, Raylib.GetScreenWidth() + RightPanelStart, Raylib.GetScreenHeight() - 250,
+            Raylib.GRAY);
+        Raylib.DrawRectangle(margin, margin, Raylib.GetScreenWidth() + RightPanelStart - 2 * margin,
+            Raylib.GetScreenHeight() - 250 - 2 * margin, Raylib.LIGHTGRAY);
 
         Raylib.DrawRectangle(Raylib.GetScreenWidth() + RightPanelStart, 0, 250, Raylib.GetScreenHeight(), Raylib.GRAY);
+        Raylib.DrawRectangle(Raylib.GetScreenWidth() + RightPanelStart + 10, 10, 230, Raylib.GetScreenHeight() - 20,
+            Raylib.LIGHTGRAY);
 
         foreach (var item in _textBoxes)
             item.Draw();
-        
+
         // Time
         _timeline.SetTimeData(_time, _timeStart, _timeEnd);
         _timeline.Draw();
-        
+
         _time += Raylib.GetFrameTime();
         if (_time > _timeEnd) _time = _timeStart;
     }

9bc0befe5fb244b948b0cd19b0e03ce28ed1c2e7
Pawel Reich <thegitman@outlook.com>
2024-05-28 00:44:43 +0200

    Added authors to IntroView

--- Puma Visualiser/Puma Visualiser/Views/IntroView.cs
+++ Puma Visualiser/Puma Visualiser/Views/IntroView.cs
@@ -9,8 +9,11 @@ internal class IntroView : IView
         Raylib.BeginDrawing();
             
         Raylib.ClearBackground(Raylib.LIGHTGRAY);
-        Raylib.DrawText("PUMA Robot Visualiser", 640 - Raylib.MeasureText("PUMA Robot Visualiser", 50) / 2, 360 - 25,
+        Raylib.DrawText("PUMA Robot Visualiser", 640 - Raylib.MeasureText("PUMA Robot Visualiser", 50) / 2, 360 - 25 - 10,
             50, Raylib.RED);
+        
+        Raylib.DrawText("Michal Sojka, Pawel Reich", 640 - Raylib.MeasureText("Michal Sojka, Pawel Reich", 30) / 2, 360 - 15 + 25 + 10,
+            30, Raylib.RED);
 
         if (Raylib.GetTime() > 1)
         {

8d0a67640044bcac7d03382b09e97e7652af3df6
Pawel Reich <thegitman@outlook.com>
2024-06-12 22:46:00 +0200

    GUI Cleanup

--- Puma Visualiser/Puma Visualiser/Views/MainView.cs
+++ Puma Visualiser/Puma Visualiser/Views/MainView.cs
@@ -1,7 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Globalization;
 using System.Numerics;
-using System.Reflection;
 using Puma_Visualiser.GuiElements;
 using Raylib_CsLo;
 
@@ -10,16 +9,18 @@ namespace Puma_Visualiser.Views;
 internal class MainView : IView
 {
     private int RightPanelStart = -250;
-    private int margin = 10;
+    private int GuiMargin = 10;
     private int RightPanelInputStartY = 100;
 
-    private List<GuiElement> _textBoxes = new List<GuiElement>();
+    private readonly List<GuiElement> _guiElements = new List<GuiElement>();
 
-    private Button _startButton;
-    private Button _stopButton;
-    private Slider _timeSlider;
+    private readonly TextBox _currentXTextBox, _currentYTextBox, _currentZTextBox, _currentTimeTextBox;
+    private readonly TextBox _targetXTextBox, _targetYTextBox, _targetZTextBox, _targetTimeTextBox;
+    
+    private readonly Button _startButton, _stopButton;
+    private readonly Slider _timeSlider;
 
-    private Timeline _timeline;
+    private readonly Timeline _timeline;
 
     private RenderTexture? robotTexture;
     private Camera3D robotCamera;
@@ -32,65 +33,94 @@ internal class MainView : IView
     private bool _timeStopped = false;
 
     private Model p1, p2, p3, p4, p5, p6, p7;
+    private Texture p1t, p2t, p3t, p4t, p5t, p6t;
     private Vector3 modelScale = new Vector3(1f, 1f, 1f);
 
     public MainView()
     {
         _timeline = new Timeline(new Rectangle(0, -250, -250, 250), Raylib.GRAY, Raylib.LIGHTGRAY, Raylib.DARKGRAY,
             Raylib.RED, 10, 10);
-        _textBoxes.Add(new TextBox(
-            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 10, 200 - 2 * margin, 30), 20));
-        _textBoxes.Add(new TextBox(
-            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 50, 200 - 2 * margin, 30), 20));
-        _textBoxes.Add(new TextBox(
-            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 90, 200 - 2 * margin, 30), 20));
-        _textBoxes.Add(new TextBox(
-            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 130, 200 - 2 * margin, 30), 20));
-
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY - 40, -RightPanelInputStartY, 40), "Current", 40, Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 10 + 5, 0, 0),
+        _guiElements.Add(_timeline);
+        
+        
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY + 130 + 20 + 40, -RightPanelInputStartY, 40), "Timestamp", 40, Raylib.BLACK));
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY + 10 + 5 + 130 + 20 + 40 + 40, 0, 0),
             "X", 20,
             Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 50 + 5, 0, 0),
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY + 50 + 5 + 130 + 20 + 40 + 40, 0, 0),
             "Y", 20,
             Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 90 + 5, 0, 0),
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY + 90 + 5 + 130 + 20 + 40 + 40, 0, 0),
             "Z", 20,
             Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 130 + 5, 0, 0),
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY + 130 + 5 + 130 + 20 + 40 + 40, 0, 0),
             "t", 20,
             Raylib.BLACK));
 
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 130 + 20 + 40, -RightPanelInputStartY, 40), "Timestamp", 40, Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 10 + 5 + 130 + 20 + 40 + 40, 0, 0),
+        /* Current values Gui Elements */
+        
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY - 40, -RightPanelInputStartY, 40), "Current", 40, Raylib.BLACK));
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY + 10 + 5, 0, 0),
             "X", 20,
             Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 50 + 5 + 130 + 20 + 40 + 40, 0, 0),
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY + 50 + 5, 0, 0),
             "Y", 20,
             Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 90 + 5 + 130 + 20 + 40 + 40, 0, 0),
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY + 90 + 5, 0, 0),
             "Z", 20,
             Raylib.BLACK));
-        _textBoxes.Add(new Label(new Rectangle(RightPanelStart + margin + 10, RightPanelInputStartY + 130 + 5 + 130 + 20 + 40 + 40, 0, 0),
+        _guiElements.Add(new Label(new Rectangle(RightPanelStart + GuiMargin + 10, RightPanelInputStartY + 130 + 5, 0, 0),
             "t", 20,
             Raylib.BLACK));
+        
+        _currentXTextBox = new TextBox(
+            new Rectangle(RightPanelStart + GuiMargin + 40, RightPanelInputStartY + 10, 200 - 2 * GuiMargin, 30), 20);
+        _guiElements.Add(_currentXTextBox);
+        
+        _currentYTextBox = new  TextBox(
+            new Rectangle(RightPanelStart + GuiMargin + 40, RightPanelInputStartY + 50, 200 - 2 * GuiMargin, 30), 20);
+        _guiElements.Add(_currentYTextBox);
+        
+        _currentZTextBox = new TextBox(
+            new Rectangle(RightPanelStart + GuiMargin + 40, RightPanelInputStartY + 90, 200 - 2 * GuiMargin, 30), 20);
+        _guiElements.Add(_currentZTextBox);
+        
+        _currentTimeTextBox = new TextBox(
+            new Rectangle(RightPanelStart + GuiMargin + 40, RightPanelInputStartY + 130, 200 - 2 * GuiMargin, 30), 20);
+        _guiElements.Add(_currentTimeTextBox);
+        
+        
+        /* Target values Gui Elements */
+        
+        _targetXTextBox = new TextBox(
+            new Rectangle(RightPanelStart + GuiMargin + 40, RightPanelInputStartY + 10 + 130 + 20 + 40 + 40, 200 - 2 * GuiMargin, 30), 20);
+        _guiElements.Add(_targetXTextBox);
+        
+        _targetYTextBox = new TextBox(
+            new Rectangle(RightPanelStart + GuiMargin + 40, RightPanelInputStartY + 50 + 130 + 20 + 40 + 40, 200 - 2 * GuiMargin, 30), 20);
+        _guiElements.Add(_targetYTextBox);
+        
+        _targetZTextBox = new TextBox(
+            new Rectangle(RightPanelStart + GuiMargin + 40, RightPanelInputStartY + 90 + 130 + 20 + 40 + 40, 200 - 2 * GuiMargin, 30), 20);
+        _guiElements.Add(_targetZTextBox);
+        
+        _targetTimeTextBox = new  TextBox(
+            new Rectangle(RightPanelStart + GuiMargin + 40, RightPanelInputStartY + 130 + 130 + 20 + 40 + 40, 200 - 2 * GuiMargin, 30), 20);
+        _guiElements.Add(_targetTimeTextBox);
 
-        _textBoxes.Add(new TextBox(
-            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 10 + 130 + 20 + 40 + 40, 200 - 2 * margin, 30), 20));
-        _textBoxes.Add(new TextBox(
-            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 50 + 130 + 20 + 40 + 40, 200 - 2 * margin, 30), 20));
-        _textBoxes.Add(new TextBox(
-            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 90 + 130 + 20 + 40 + 40, 200 - 2 * margin, 30), 20));
-        _textBoxes.Add(new TextBox(
-            new Rectangle(RightPanelStart + margin + 40, RightPanelInputStartY + 130 + 130 + 20 + 40 + 40, 200 - 2 * margin, 30), 20));
-
-        _startButton = new Button(new Rectangle(RightPanelStart + margin + 10, -10 - margin - 40 - 10 - 40, -RightPanelStart - 3 * margin - 10, 40), "START");
-        _stopButton = new Button(new Rectangle(RightPanelStart + margin + 10, -10 - margin - 40, -RightPanelStart - 3 * margin - 10, 40), "STOP");
+        
+        _startButton = new Button(new Rectangle(RightPanelStart + GuiMargin + 10, -10 - GuiMargin - 40 - 10 - 40, -RightPanelStart - 3 * GuiMargin - 10, 40), "START");
+        _guiElements.Add(_startButton);
+        
+        _stopButton = new Button(new Rectangle(RightPanelStart + GuiMargin + 10, -10 - GuiMargin - 40, -RightPanelStart - 3 * GuiMargin - 10, 40), "STOP");
+        _guiElements.Add(_stopButton);
+        
         _timeSlider = new Slider(
-            new Rectangle(RightPanelStart + margin + 10, -10 - margin - 40 - 40 - 30 - 10,
-                -RightPanelStart - 3 * margin - 10, 20), _minTimeScale.ToString(CultureInfo.InvariantCulture),
+            new Rectangle(RightPanelStart + GuiMargin + 10, -10 - GuiMargin - 40 - 40 - 30 - 10,
+                -RightPanelStart - 3 * GuiMargin - 10, 20), _minTimeScale.ToString(CultureInfo.InvariantCulture),
             _maxTimeScale.ToString(CultureInfo.InvariantCulture), _minTimeScale, _maxTimeScale, _timeScale, 20);
-
+        _guiElements.Add(_timeSlider);
+        
         InitializeRobot();
     }
 
@@ -116,20 +146,9 @@ internal class MainView : IView
         Raylib.EndTextureMode();
 
 
-        // GUI DRAWING
-
-        Raylib.BeginDrawing();
-
-        Raylib.ClearBackground(Raylib.GRAY);
-
-        Raylib.DrawRectangle(Raylib.GetScreenWidth() + RightPanelStart, 0, 250, Raylib.GetScreenHeight(), Raylib.GRAY);
-        Raylib.DrawRectangle(Raylib.GetScreenWidth() + RightPanelStart + 10, 10, 230, Raylib.GetScreenHeight() - 20,
-            Raylib.LIGHTGRAY);
-
-        foreach (var item in _textBoxes)
-            item.Draw();
-
-        // Time
+        // TIME
+        
+        _timeScale = _timeSlider.GetValue();
         _timeline.SetTimeData(_time, _timeStart, _timeEnd);
         _timeline.Draw();
 
@@ -144,12 +163,22 @@ internal class MainView : IView
         if (!_timeStopped)
             _time += Raylib.GetFrameTime() * _timeScale;
         if (_time > _timeEnd) _time = _timeStart;
+        
+        // GUI DRAWING
 
-        Raylib.DrawTexture(robotTexture.Value.texture, margin, margin, Raylib.WHITE);
+        Raylib.BeginDrawing();
 
-        _timeSlider.Draw();
-        _timeScale = _timeSlider.GetValue();
+        Raylib.ClearBackground(Raylib.GRAY);
 
+        Raylib.DrawRectangle(Raylib.GetScreenWidth() + RightPanelStart, 0, 250, Raylib.GetScreenHeight(), Raylib.GRAY);
+        Raylib.DrawRectangle(Raylib.GetScreenWidth() + RightPanelStart + 10, 10, 230, Raylib.GetScreenHeight() - 20,
+            Raylib.LIGHTGRAY);
+        
+        foreach (var item in _guiElements)
+            item.Draw();
+        
+        Raylib.DrawTexture(robotTexture.Value.texture, GuiMargin, GuiMargin, Raylib.WHITE);
+        
         Raylib.EndDrawing();
     }
 
@@ -158,9 +187,9 @@ internal class MainView : IView
         if (robotTexture.HasValue)
             Raylib.UnloadRenderTexture(robotTexture.Value);
 
-        // Robot render
-        robotTexture = Raylib.LoadRenderTexture(Raylib.GetScreenWidth() + RightPanelStart - 2 * margin,
-            Raylib.GetScreenHeight() - 250 - 2 * margin);
+        // Robot render 
+        robotTexture = Raylib.LoadRenderTexture(Raylib.GetScreenWidth() + RightPanelStart - 2 * GuiMargin,
+            Raylib.GetScreenHeight() - 250 - 2 * GuiMargin);
         robotCamera = new Camera3D(new Vector3(200, 200, 200), Vector3.Zero, -Vector3.UnitY, 45f,
             CameraProjection.CAMERA_PERSPECTIVE);
         
@@ -172,34 +201,20 @@ internal class MainView : IView
         p5 = Raylib.LoadModel("resources\\p4.obj");
         p6 = Raylib.LoadModel("resources\\p5.obj");
         p7 = Raylib.LoadModel("resources\\kostka.obj");
+        
+        p1t = Raylib.LoadTexture("resources\\p1.png");
+        Raylib.SetTextureWrap(p1t, TextureWrap.TEXTURE_WRAP_CLAMP);
+        
+        unsafe
+        {
+            p1.materials[0].maps[0].texture = p1t;
+        }
     }
 
     private float theta1 = 0.0f, theta2 = 0.0f, theta3 = 0.0f, theta4 = 0.0f, theta5 = 0.0f, theta6 = 0.0f;
 
     private void DrawRobot()
     {
-        /*chcialem przerobic kod  na ladniejszy ale nie pyklo
-
-        float pos03x = 41.917f * c1 * c2 - 12.92f * s1;
-        float pos03y = 41.917f * s1 * c2 + 12.92f * c1;
-        float pos03z = 61.722f + 41.917f * s2;
-
-        float pos06x = pos03x + 43.215f * (c1 * c2 * s3 + c1 * s2 * c3);
-        float pos06y = pos03y + 43.215f * (s1 * c2 * s3 + s1 * s2 * c3);
-        float pos06z = pos03z + 43.215f * (s2 * s3 - c2 * c3);
-
-        float pos7x = pos03x + (43.215f + c5 * 5.334f) * (c1 * c2 * s3 + c1 * s2 * c3);
-        float pos7y = pos03y + (43.215f + c5 * 5.334f) * (s1 * c2 * s3 + s1 * s2 * c3);
-        float pos7z = pos03z + (43.215f + c5 * 5.334f) * (s2 * s3 - c2 * c3);
-
-        float pos07x = pos7x - 5.334f * c4 * s5 * (c1 * c2 * c3 - c1 * s2 * s3) - 5.334f * s4 * s5 * s1;
-        float pos07y = pos7y + 5.334f * c4 * s5 * (s1 * c2 * c3 - s1 * s2 * s3) + 5.334f * s4 * s5 * c1;
-        float pos07z = pos7z - 5.334f * c4 * s5 * (s2 * c3 - c2 * s3);
-        Vector3 pos03 = new Vector3(pos03x, pos03z, pos03y);
-        Vector3 pos06 = new Vector3(pos06x, pos06z, pos06y);
-        Vector3 pos07 = new Vector3(pos07x, pos07z, pos07y);
-*/
-
         //translation of DH position
 
         float pos03x = 41.917f * MathF.Cos(RayMath.DEG2RAD * theta1) * MathF.Cos(RayMath.DEG2RAD * theta2) - 12.92f * MathF.Sin(RayMath.DEG2RAD * theta1);
@@ -292,18 +307,10 @@ internal class MainView : IView
             if (theta6 <= 150)
                 theta6 += 1f;
             else;
-
-        //axis rotation points
-        /*p3.transform = RayMath.MatrixTranslate(0, 0, 0);
-        p4.transform = RayMath.MatrixTranslate(0, 0, 0);
-        p5.transform = RayMath.MatrixTranslate(0, 0, 0);
-        p6.transform = RayMath.MatrixTranslate(0, 0, 0);*/
-
+        
         p2.transform = RayMath.MatrixRotateXYZ(new Vector3(0f, RayMath.DEG2RAD * theta1, 0f));
         p3.transform = RayMath.MatrixRotateXYZ(new Vector3(RayMath.DEG2RAD * theta2, RayMath.DEG2RAD * theta1, 0f));
         p4.transform = RayMath.MatrixRotateXYZ(new Vector3(RayMath.DEG2RAD * theta2 + RayMath.DEG2RAD * theta3, RayMath.DEG2RAD * theta1, 0f));
-
-        //p5.transform = RayMath.MatrixRotateXYZ(new Vector3(-RayMath.DEG2RAD * theta2 - RayMath.DEG2RAD * theta3, RayMath.DEG2RAD * theta1 + RayMath.DEG2RAD * theta4, 0f));
 
         Matrix4x4 p5previousTransform = RayMath.MatrixRotateXYZ(new Vector3(-RayMath.DEG2RAD * theta2 - RayMath.DEG2RAD * theta3, RayMath.DEG2RAD * theta1, 0f));
         Matrix4x4 p5localTransform = RayMath.MatrixRotateY(RayMath.DEG2RAD * theta4);
@@ -318,11 +325,6 @@ internal class MainView : IView
         Matrix4x4 p7additionalTransform = RayMath.MatrixRotateY(RayMath.DEG2RAD * theta6);
         Matrix4x4 p7LocalTransform = RayMath.MatrixMultiply(p6LocalTransform, p7additionalTransform);
         p7.transform = RayMath.MatrixMultiply(p6previousTransform, p7LocalTransform);
-
-        //p6.transform = RayMath.MatrixRotateXYZ(new Vector3(RayMath.DEG2RAD * theta2 + RayMath.DEG2RAD * theta3 + RayMath.DEG2RAD * theta5, RayMath.DEG2RAD * theta1 + RayMath.DEG2RAD * theta4, 0f));
-        //if(isitemgrabbed(true)) item.transform = RayMath.MatrixRotateXYZ(new Vector3(RayMath.DEG2RAD * theta2 + RayMath.DEG2RAD * theta3 RayMath.DEG2RAD * theta5, RayMath.DEG2RAD * theta1 + RayMath.DEG2RAD * theta4RayMath.DEG2RAD * theta6, 0f));
-
-
     }
     
 
